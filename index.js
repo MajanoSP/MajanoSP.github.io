8865829300845a2c20658afc2e449dbf3b6149aa
MajanoSP <138042669+MajanoSP@users.noreply.github.com>
2023-07-18 20:10:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const boton1= document.getElementById("boton-1");
+
+boton1.addEventListener("click",()=>{
+    window.location ="sandwich.html"
+});
+
+const boton2= document.getElementById("boton-2");
+
+boton2.addEventListener("click",()=>{
+    window.location ="burgers.html"
+});
+
+const boton3= document.getElementById("boton-3");
+
+boton3.addEventListener("click",()=>{
+    window.location ="pizzas.html"
+})
